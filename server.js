babd207174f18600373baa7f1d4a6d829c50a38a
arseniojr <140646846+arseniojr@users.noreply.github.com>
2023-07-27 18:22:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /*********************************************************************************
-* WEB700 – Assignment 04
+* WEB700 – Assignment 05
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part 
 * of this assignment has been copied manually or electronically from any other source 
 * (including 3rd party web sites) or distributed to other students.
 * 
 * Name: ARSENIO JR ROMRRO  Student ID: 149873218  Date: JULY 27, 2023
 *
-* Online (Cyclic) Link: ________________________________________________________
+* Online (Cyclic) Link: https://vivacious-ox-knickers.cyclic.app/
 *
 ********************************************************************************/
 
